ec8ff70f407ffe16a97d33713d26a08bb9e4e430
shaul ben david <shaul.bendavid@abra-it.com>
2023-03-09 16:54:52 +0200

    initial project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { goto, beforeNavigate, afterNavigate } from '$app/navigation';
+	const handleClick = () => {
+		goto('/products', { replaceState: true });
+	};
+	beforeNavigate(() => {
+		console.log('before');
+	});
+
+	afterNavigate(() => {
+		console.log('after');
+	});
+</script>
+
+<h1>Hello world</h1>
+<a href="/blog">Blog</a>
+<a href="/products">Products</a>
+<button on:click={handleClick}>place holder</button>
---
+++ src/routes/products/[productId]/reviews/[reviewId]/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	import { page } from '$app/stores';
+	const { reviewId, productId } = $page.params;
+</script>
+
+<h1>Review {reviewId} for product {productId}</h1>
