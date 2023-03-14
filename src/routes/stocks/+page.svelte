e06b87fbeaf7ace0db35cd1ba0f622e0d7b5f3db
shaul ben david <shaul.bendavid@abra-it.com>
2023-03-14 16:46:52 +0200

    adding

--- src/routes/stocks/+page.svelte
+++ src/routes/stocks/+page.svelte
@@ -6,9 +6,13 @@
 		invalidate('stocks');
 		invalidateAll();
 	}
+	let count = 0;
+	$: counter = () => (count += 1);
+	console.log('1 =', 1);
 </script>
 
 <h1>{stock.wixStock.symbol}</h1>
 <h1>{stock.appleStock.symbol}</h1>
 <h1>{stock.teslaStock.symbol}</h1>
 <button on:click={refresh}>Refresh</button>
+<button on:click={counter}>{count}</button>
